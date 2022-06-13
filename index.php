ae0a483c487a1ab45b5b26f43d74bffd1e07c039
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-13 20:37:13 -0300

    update_form

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
 <?php
 echo "Hellow World";
 ?>
-</body>   
+<form action="action.php" method="post">
+    <p>Yout name: <input type="text" name="name" /></p>
+    <p>Yout age: <input type="text" name="age" /></p>
+    <p><input type="submit" /></p>
+</form>
+
+</body> 
 </html>
