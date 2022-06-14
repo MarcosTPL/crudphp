dc455ebb4d1927d018c24bd4edbf7bb8252fdd4d
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-14 19:57:00 -0300

    update

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>My Project</title>
-    </head>
-    <body>
-<?php
-echo "Hellow World";
-?>
-<form action="action.php" method="post">
-    <p>Yout name: <input type="text" name="name" /></p>
-    <p>Yout age: <input type="text" name="age" /></p>
-    <p><input type="submit" /></p>
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-</body> 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Contato</title>
+  </head>
+  <body>
+  
+      <section>
+
+        <div class="contato">
+            <h3>Formulário de contato</h3>
+            <form>
+              <input class="field" name="name" placeholder="Nome">
+              <input class="field" name = "email" placeholder="E-mail">
+              <textarea class="field" name="message" placeholder="Digite sua mensagem aqui."></textarea>
+              <input type="submit" value= "Envias">
+            </form>
+        </div>
+      </section>
+
+
+
+</div>
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
