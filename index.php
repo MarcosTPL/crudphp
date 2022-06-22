04cf01f66d40b6e5f63112eec60bd4aa901d35cb
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-22 20:20:04 -0300

    update-22/06-20:20

--- index.php
+++ index.php
@@ -8,7 +8,33 @@
     <link rel="stylesheet" type="text/css" href="css/reset.css">
     <link rel="stylesheet" type="text/css" href="css/style-index.css">
 </head>
-
+<style>
+body{
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: linear-gradient(to right, rgb(20, 147, 228), rgb(17, 54, 71));
+    text-align: center;
+    color: white;
+}
+.box{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 30px;
+    border-radius: 10px;
+}
+a{
+    text-decoration: none;
+    color: white;
+    border: 3px solid dodgerblue;
+    border-radius: 10px;
+    padding: 10px;
+}
+a:hover{
+    background-color: dodgerblue;
+}
+</style>
 <body>
     <h1>Index</h1>
     <div class="box">
