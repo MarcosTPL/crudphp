f6c74d00b0b753e49d4c1cee4ec576cad6d02f0e
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-22 19:58:52 -0300

    update-22/06-19:58

--- index.php
+++ index.php
@@ -7,9 +7,14 @@
     <title>Index</title>
     <link rel="stylesheet" type="text/css" href="css/reset.css">
     <link rel="stylesheet" type="text/css" href="css/style-index.css">
-
-
 </head>
+<style>body{
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: linear-gradient(to right, rgb(20, 147, 228), rgb(17, 54, 71));
+    text-align: center;
+    color: white;
+}
+</style>
 <body>
     <h1>Index</h1>
     <div class="box">
