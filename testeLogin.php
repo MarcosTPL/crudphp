736748eca8b6256a14a1dc088f45fbe6d5cbbbbe
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-22 23:38:21 -0300

    update-22/06-23:38

--- testeLogin.php
+++ testeLogin.php
@@ -1,13 +1,25 @@
 <?php
 
     //print_r($_REQUEST);
-    if(isset($_POST['submit']))
+    if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
     {
+        // Acessa
+        include_once('config.php');
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
 
+        // print_r('Email: ' . $email);
+        // print_r('<br>');
+        // print_r('Senha: ' . $senha);
+
+
+        $result = $conexao->query("SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'");
+
+        print_r($result);
     }
     else
     {
-        //header('Location: login.php');
+        // Não acessa
         header('Location: login.php');
     }
 
