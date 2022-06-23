28beecc8360edafd60fa15ddcc761d283e2be0af
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-22 23:21:35 -0300

    update-22/06-23:21

---
+++ testeLogin.php
@@ -0,0 +1,14 @@
+<?php
+
+    //print_r($_REQUEST);
+    if(isset($_POST['submit']))
+    {
+
+    }
+    else
+    {
+        //header('Location: login.php');
+        header('Location: login.php');
+    }
+
+?>
