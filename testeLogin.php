72f77abcb2cba90cfa4fa40dda3db7bb51325180
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-26 11:56:10 -0300

    update-26/06-(testeLogin,sistema)

--- testeLogin.php
+++ testeLogin.php
@@ -18,11 +18,11 @@
 
         if(mysqli_num_rows($result) < 1)
         {
-            
+            header('Location: login.php');
         }
         else
         {
-            
+            header('Location: sistema.php');
         }
     }
     else
