1435f4dfe97f8244612ff4c7f111d90575c24574
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-26 13:28:44 -0300

    update-26/06-(sistema,style-sistema)

--- sistema.php
+++ sistema.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    print_r($_SESSION);
+    // print_r($_SESSION);
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
     {   
         unset($_SESSION['email']);
@@ -15,9 +15,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>SISTEMA  | GN</title>
+    <link rel="stylesheet" type="text/css" href="css/style-sistema.css">
 </head>
 <body>
-    <h1>Acessou o sistema</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SISTEMA DO GN</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="d-flex">
+            <a href="" class="btn btn-danger me-5">Sair</a>
+        </div>
+    </nav>
+    <?php
+        echo "<h1>Bem vindo <u>$logado</u></h1>";
+    ?>
 </body>
 </html>
