2726f554ec9aa0796cf6e9e874a5953687d70b81
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-26 16:41:22 -0300

    update-26/06-(sistema)

--- sistema.php
+++ sistema.php
@@ -49,5 +49,44 @@
     <?php
         echo "<h1>Bem vindo <u>$logado</u></h1>";
     ?>
+    <div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Senha</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Sexo</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Cidade</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Enderelo</th>
+                <th scope="col">...</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                while($user_data = mysqli_fetch_assoc($result))
+                {
+                    echo "<tr>";
+                    echo "<td>" .$user_data['id']. "</td>";
+                    echo "<td>" .$user_data['nome']. "</td>";
+                    echo "<td>" .$user_data['email']. "</td>";
+                    echo "<td>" .$user_data['senha']. "</td>";
+                    echo "<td>" .$user_data['telefone']. "</td>";
+                    echo "<td>" .$user_data['sexo']. "</td>";
+                    echo "<td>" .$user_data['data_nasc']. "</td>";
+                    echo "<td>" .$user_data['cidade']. "</td>";
+                    echo "<td>" .$user_data['estado']. "</td>";
+                    echo "<td>" .$user_data['endereco']. "</td>";
+                    echo "<td>ações</td>";
+                    echo "<tr>";
+                }
+            ?>
+        </tbody>
+</table>
+    </div>
 </body>
 </html>
