6296648099e6ecbb30e397a523162e55e8e73c54
MarcosTPL <tulinhoplima15@gmail.com>
2022-06-26 22:56:43 -0300

    update-26/06-(sistema)

--- sistema.php
+++ sistema.php
@@ -24,13 +24,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>SISTEMA  | GN</title>
-    <link rel="stylesheet" type="text/css" href="css/style-sistema.css">
     <style>
         body{
         background-image: linear-gradient(to right, rgb(20, 147, 228), rgb(17, 54, 71));
-        color:white;
+        color: white;
         text-align: center;
         }
+        thead{
+            border-color: white;
+        }
+        
     </style>
 </head>
 <body>
@@ -50,7 +53,7 @@
         echo "<h1>Bem vindo <u>$logado</u></h1>";
     ?>
     <div>
-    <table class="table">
+    <table class="table" >
         <thead>
             <tr>
                 <th scope="col">#</th>
@@ -71,8 +74,8 @@
                 while($user_data = mysqli_fetch_assoc($result))
                 {
                     echo "<tr>";
-                    echo "<td>" .$user_data['id']. "</td>";
-                    echo "<td>" .$user_data['nome']. "</td>";
+                    echo "<td>" . $user_data['id'] . "</td>";
+                    /*echo "<td>" .$user_data['nome']. "</td>";
                     echo "<td>" .$user_data['email']. "</td>";
                     echo "<td>" .$user_data['senha']. "</td>";
                     echo "<td>" .$user_data['telefone']. "</td>";
@@ -82,7 +85,8 @@
                     echo "<td>" .$user_data['estado']. "</td>";
                     echo "<td>" .$user_data['endereco']. "</td>";
                     echo "<td>ações</td>";
-                    echo "<tr>";
+                    */echo "<tr>";
+                    
                 }
             ?>
         </tbody>
